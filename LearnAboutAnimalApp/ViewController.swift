5cf88aeb8ba7128b0672d35fd8f57aa8c683a5cd
Sahi Joshi <sahik.joshi@gmail.com>
2020-12-31 05:57:23 +0200

    Initial commit

---
+++ LearnAboutAnimalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnAboutAnimalApp
+//
+//  Created by skj on 31.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
